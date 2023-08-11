4820e4b5203d687b2d56139fb51a821a7d18ac2f
ozen-hub <info.ozenhub@gmail.com>
2023-08-11 21:17:44 +0530

    1

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -1,7 +1,11 @@
+import com.sun.xml.internal.bind.v2.model.core.ID;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
+import java.util.regex.Pattern;
 
 public class MainFormController {
     public TextField txtId;
@@ -14,7 +18,46 @@ public class MainFormController {
     public TableColumn colAddress;
     public TableColumn colSalary;
 
+    private boolean validateFields(TextField field, Pattern pattern){
+        if (pattern.matcher(field.getText()).matches()){
+            field.setStyle("");
+            return true;
+        }else{
+            field.setStyle("-fx-border-color: red;");
+            return false;
+        }
+    }
+    private boolean validate() {
+        final String ID_REG = "\\d-\\d"; //D-1***
+        final String NAME_REG = ".{3,}";
+        final String ADDRESS_REG = ".+";
+        final String SALARY_REG = "\\d+(\\.\\d{1,2})?";
+
+        Pattern idP=Pattern.compile(ID_REG);
+        Pattern nameP=Pattern.compile(NAME_REG);
+        Pattern addressP=Pattern.compile(ADDRESS_REG);
+        Pattern salaryP=Pattern.compile(SALARY_REG);
+
+        if (
+                validateFields(txtId,idP) &&
+                        validateFields(txtName,nameP) &&
+                        validateFields(txtAddress, addressP) &&
+                        validateFields(txtSalary, salaryP)
+        ){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
     public void btnSaveOnAction(ActionEvent actionEvent) {
+        if (validate()){
+            // save
+        }else{
+            new Alert(Alert.AlertType.WARNING, "Please insert valid data!").show();
+        }
+
     }
 
     public void btnPrintAll(ActionEvent actionEvent) {
