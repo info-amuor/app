c1a489ab3a685f34e63cf2a6a75b8d5a0e8343ad
ozen-hub <info.ozenhub@gmail.com>
2023-08-11 20:54:39 +0530

    1

--- src/MainFormController.java
+++ src/MainFormController.java
@@ -1,2 +1,22 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+    public TableView<CustomerTM> tableCustomer;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPrintAll(ActionEvent actionEvent) {
+    }
 }
